52151d29da92f69396234f2bb3c10b1d927a8308
Thieu Le Quang <quangthieuagu@gmail.com>
2017-05-21 22:47:30 +0700

    add laradocker

--- db/build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-mysql -h mysql -u$MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE < /data/www/db/migrations/rms_2017-03-23.sql
--- www/public/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 5/16/17
- * Time: 12:00 AM
- */
-phpinfo();
